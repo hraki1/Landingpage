f4c5a1f0d8bcbe0b7e3765771b793e8b222fb308
ahmad alhraki <ahmadhraki752@gmail.com>
2025-07-04 13:58:41 +0300

    ffdf

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,11 @@
 import createMiddleware from "next-intl/middleware";
-import type { NextRequest } from "next/server";
 import { routing } from "./i18n/routing";
 
-const intlMiddleware = createMiddleware(routing);
+export default createMiddleware(routing);
 
-export default function middleware(request: NextRequest) {
-  const response = intlMiddleware(request);
-
-  // intlMiddleware ممكن يرجع Response أو Promise<Response>
-  // نتأكد من النوع ونتعامل معه بشكل صحيح:
-  if (response instanceof Promise) {
-    return response.then((res) => {
-      const locale = request.nextUrl.locale || routing.defaultLocale;
-
-      res.cookies.set("NEXT_LOCALE", locale, {
-        maxAge: 60 * 60 * 24 * 365,
-        path: "/",
-        secure: true,
-        sameSite: "lax",
-      });
-
-      return res;
-    });
-  } else {
-    const locale = request.nextUrl.locale || routing.defaultLocale;
-
-    response.cookies.set("NEXT_LOCALE", locale, {
-      maxAge: 60 * 60 * 24 * 365,
-      path: "/",
-      secure: true,
-      sameSite: "lax",
-    });
-
-    return response;
-  }
-}
+export const config = {
+  // Match all pathnames except for
+  // - … if they start with ``, `/trpc`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
+};
